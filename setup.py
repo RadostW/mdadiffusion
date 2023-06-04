8fd00bc7205218fcda013b988375b46651abf90e
RadostW <radost.waszkiewicz@gmail.com>
2023-06-04 14:02:20 +0200

    Bump verion. setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup
 with open('README.md') as f:
     long_description = f.read()
 
-setup(name='MDA diffusion',
-      version='0.0.0',
+setup(name='mdadiffusion',
+      version='0.0.1',
       description='Minimum dissipation approximation for hydrodynamic size.',
-      url='https://github.com/RadostW/MininumDissipationApproximation/',
+      url='https://github.com/RadostW/mdadiffusion/',
       author='Radost Waszkiewicz',
       author_email='radost.waszkiewicz@gmail.com',
       long_description=long_description,
       long_description_content_type='text/markdown',  # This is important!
       project_urls = {
-          'Documentation': 'https://mdadiffusion.readthedocs.io',
-          'Source': 'https://github.com/RadostW/minimumdissipationapproximation/'
+          # 'Documentation': 'https://mdadiffusion.readthedocs.io', # TODO
+          'Source': 'https://github.com/RadostW/mdadiffusion/'
       },
-      license='GNU GPLv3',
+      license='MIT',
       packages=['mdadiffusion'],
+      install_requires=['numpy>1.16','pygrpy','sarw_spheres'],
       zip_safe=False)
